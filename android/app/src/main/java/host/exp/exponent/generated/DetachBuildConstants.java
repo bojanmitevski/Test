2078a01fdb093dc3e0e24be7c39eef2a38547712
Bojan Mitevski <bokidev@MacBook-Pro-van-user.local>
2018-08-29 10:56:58 +0200

    init

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "expeb25366181824b328bfacd4ae0054bca://192.168.1.253:80";
+
+}
